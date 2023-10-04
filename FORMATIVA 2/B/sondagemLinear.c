e9ff47288caff3c0aead50af834101dba4627256
cwtshh <gucosta1719@gmail.com>
2023-10-04 13:34:28 -0300

    sondagem linear

--- FORMATIVA 2/B/sondagemLinear.c
+++ FORMATIVA 2/B/sondagemLinear.c
@@ -3,59 +3,73 @@
 
 typedef struct {
     int *tb;
-    int M;
-    int N;
+    int M; 
+    int N; 
 } TH;
 
-int hashModular (int ch, int M) {
+int aumentaTamanho(int M) {}
+
+int hash(int ch, int M) {
     return ch % M;
 }
 
 void THinsere (TH *h, int ch) {
-    int position = hashModular(ch, h->M);
-
     if(h->N > (h->M/2)) {
-        int novoTamanho = aumentaTamanho(h->M);
-        h->M = novoTamanho;
-        h->tb = realloc(h->tb, novoTamanho * sizeof(int));
+        h->M = aumentaTamanho(h->M);
     }
+
+    int position = hash(ch, h->M);
 
     if(h->tb[position] == -1) {
         h->tb[position] = ch;
-        h->N++;
     } else {
-        for(int i = position; i <= h->M; i++) {
+        for(int i = position; i < h->M; i++) {
             if(h->tb[i] == -1) {
                 h->tb[i] = ch;
-                h->N++;
-                return;
-            }
-
-            if(i == h->M) {
-                i = 0;
+                break;
             }
         }
     }
 }
 
-int THremove (TH *h, int ch) {
-    for(int i = 0; i <= h->M; i++) {
-        if(h->tb[i] == ch) {
-            h->tb[i] = -1;
-            h->N--;
-            return 0;
-        }
-    } 
+int THremove(TH *h, int ch) {
+    int position = hash(ch, h->M);
 
+    if(h->tb[position] == ch) {
+        h->tb[position] = -1;
+
+        return 0;
+    } else {
+        for(int i = position; i < h->M; i++) {
+            if(h->tb[i] == ch) {
+                h->tb[i] = -1;
+                return 0;
+            }
+
+            if(h->tb[i] == -1) {
+                return -1;
+            }
+        }
+    }
     return -1;
 }
 
 int THbusca (TH *h, int ch) {
-    for(int i = 0; i <= h->M; i++) {
-        if(h->tb[i] == ch) {
-            return 1;
+    int position = hash(ch, h->M);
+
+    if(h->tb[position] == ch) {
+        return position;
+    } else {
+        for(int i = position; i < h->M; i++) {
+            if(h->tb[i] == ch) {
+                return i;
+            }
+
+            if(h->tb[i] == -1) {
+                return 0;
+            }
         }
     }
-
     return 0;
 }
+
